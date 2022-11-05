93731d1b0caa6b1e593bb373efcf0587d851cec8
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2022-11-05 06:13:23 +0100

    .

--- LloydMax_quantization.py
+++ LloydMax_quantization.py
@@ -13,10 +13,7 @@ logger.setLevel(logging.WARNING)
 #logger.setLevel(logging.DEBUG)
 
 import numpy as np
-#from sklearn import cluster
-from sklearn.cluster import KMeans
-from sklearn.utils import shuffle
-from sklearn_extra.cluster import KMedoids
+from scipy.ndimage import uniform_filter1d
 from quantization import Quantizer
 import warnings
 
@@ -24,103 +21,80 @@ name = "Lloyd-Max"
 
 class LloydMax_Quantizer(Quantizer):
 
-    def __init__(self, x, Q_step, min_val=0, max_val=255, speedme=False, use_medoid=False, N_samples=1_000, metric='euclidean'):
-        '''Creates the classifier using the samples in <x>. <Q_step>
-        is the quantization step size, <min_val> is the minimum
-        expected value faced by the classifier, and <max_val> the
-        maximum value. Notice that <Q_step> is used only for providing
-        an initial value for the centroids (the distance between the
-        representation levels is not going to be <Q_step> in
-        general). <speedme> decreases significantly the processing
-        time at the cost of a lower performence. If <use_medoid> is
-        False, the representation levels are computed as K-means's
-        centroids (a mean). Otherwise, one of the elements of the
-        cluster is selected as the representation level (a
-        medoid). <n_samples> is used to run K-medoids (in the case of
-        K-means it is ignored) <metric> is a string, or
-        callable. Accepted metrics are listed by
-        sklearn.metrics.pairwise_distances_argmin(). The callable
-        function must input two points and output a scalar (with the
-        value of the distance between them).
+    def __init__(self, Q_step, counts, min_val=0, max_val=255, max_iters=100, epsilon=1E5):
+        '''Defines the working parameters of the quantizer:
+
+        Q_step: quantization step size.
+
+        counts: number of ocurrences of each possible input
+        sample. None of such ocurrences should be 0 (although this
+        issue is solved in this constructor). This will generate that
+        the enumeration of the quantization indexes are not
+        consecutive, and usually, will not start at 0 (see the
+        notebook), but this should not be a problem for those entropy
+        codecs that do not expect to find an input in the range [0,
+        ...]. Notice, anyway, that the used centroids (the number of
+        different quantization indexes used by the encoder in the
+        quantization of the signal, that can be smaller than the
+        number of bins) must be sent to the decoder.
+        
+        [min_val, max_val]: expected dynamic range of the input
+        signal.
+
+        max_iters: maximum number of iterations.
 
         '''
         super().__init__(Q_step, min_val, max_val)
-        self.N_clusters = (max_val + 1 - min_val) // Q_step
-        if use_medoid == False:
-            self.algorithm = "Kmeans"
-            if speedme:
-                initial_decision_levels = np.linspace(min_val, max_val + 1, self.N_clusters + 1).reshape(-1, 1)
-                initial_centroids = 0.5 * (initial_decision_levels[1:] + initial_decision_levels[:-1])
-                logger.info(f"initial_centroids={initial_centroids.squeeze()}")
+        self.N_bins = (max_val + 1 - min_val) // Q_step
+        initial_boundaries = np.linspace(min_val, max_val + 1, self.N_bins + 1)
+        initial_centroids = 0.5 * (initial_boundaries[1:] + initial_boundaries[:-1])
+        self.centroids = initial_centroids
+        logger.info(f"initial_centroids={self.centroids}")
+        self.boundaries = initial_boundaries
+        logger.info(f"initial_boundaries={self.boundaries}")
+        prev_b = np.zeros(self.boundaries.size)
+        for j in range(max_iters):
+            prev_b[:] = self.boundaries
+            self._compute_boundaries()
+            max_abs_error = np.max(np.abs(prev_b - self.boundaries))
+            if (j>0) and (max_abs_error <= epsilon):
+                break
+            self._compute_centroids(counts)
+        logger.info(f"centroids={self.centroids}")
 
-                self.classifier = KMeans(n_clusters=self.N_clusters, init=initial_centroids, n_init=1)
-            else:
-                self.classifier = KMeans(n_clusters=self.N_clusters)
-            self.train(x)
-        else:
-            self.algorithm = "KMedoids"
-            #self.classifier = KMedoids(n_clusters=self.N_clusters, random_state=0, init="random")
-            #self.classifier = KMedoids(n_clusters=self.N_clusters, init="random")
-            #self.classifier = KMedoids(n_clusters=self.N_clusters, init="heuristic")
-            #self.classifier = KMedoids(n_clusters=self.N_clusters, init="k-medoids++")
-            #self.classifier = KMedoids(n_clusters=self.N_clusters, init="build")
-            #self.classifier = KMedoids(n_clusters=self.N_clusters, method="pam")
-            self.classifier = KMedoids(init="k-medoids++", n_clusters=self.N_clusters)
-            sampled_x = shuffle(x.flatten(), random_state=0, n_samples=N_samples).reshape((-1, 1))
-            self.classifier.fit(sampled_x)
-            #self.train(shuffle(x.flatten(), random_state=0, n_samples=N_samples))
-            #for i in range(x.shape[0]):
-                #self.train(x[i])
-                #self.classifier.fit(x[i].reshape((-1, 1)))
-            self._sort_labels()
-                
-    def _sort_labels(self):
-        self.centers = self.classifier.cluster_centers_.squeeze()
-        idx = np.argsort(self.classifier.cluster_centers_.sum(axis=1))
-        self.lut = np.zeros_like(idx)
-        self.lut[idx] = np.arange(self.N_clusters)
-        logger.info(f"lut={self.lut}")
-        logger.info(f"centroids={self.centers}")
-        argsort_lut = np.argsort(self.lut)
-        sorted_centroids = self.centers[argsort_lut]
-        logger.info(f"sorted_centroids={sorted_centroids}")
-        logger.info(f"labels={self.classifier.labels_} len={len(self.classifier.labels_)}")
-        sorted_labels = self.lut[self.classifier.labels_]
-        logger.info(f"sorted_labels={sorted_labels} len={len(sorted_labels)}")
-        self.centers[:] = sorted_centroids
-        self.classifier.labels_ = sorted_labels
-        
-    def train(self, x):
-        flatten_x = shuffle(x.reshape((-1, 1)), random_state=0, n_samples=x.size)
-        self.classifier.fit(flatten_x)
-        self.centers = self.classifier.cluster_centers_.squeeze()
-        idx = np.argsort(self.classifier.cluster_centers_.sum(axis=1))
-        self.lut = np.zeros_like(idx)
-        self.lut[idx] = np.arange(self.N_clusters)
-        logger.info(f"lut={self.lut}")
-        logger.info(f"centroids={self.centers}")
-        argsort_lut = np.argsort(self.lut)
-        sorted_centroids = self.centers[argsort_lut]
-        logger.info(f"sorted_centroids={sorted_centroids}")
-        logger.info(f"labels={self.classifier.labels_} len={len(self.classifier.labels_)}")
-        sorted_labels = self.lut[self.classifier.labels_]
-        logger.info(f"sorted_labels={sorted_labels} len={len(sorted_labels)}")
-        self.centers[:] = sorted_centroids
-        self.classifier.labels_ = sorted_labels
-
-    def retrain(self, x):
-        '''Retrain the classifier using previous centers.'''
-        if self.algorithm == "KMeans":
-            self.classifier = KMeans(n_clusters=self.N_clusters, init=self.classifier.cluster_centers_, n_init=1)
-        self.train(x)
-
-    def encode(self, x):
-        '''Use the created classifier to find the quantization indexes
-        (labels) for each point of <x>.
+    def _compute_boundaries(self):
+        '''The new boundaries are in the middle points between the
+        current centroids.
 
         '''
-        k = self.classifier.predict(x.reshape((-1, 1)))
-        k.shape = x.shape
+        logger.debug(f"centroids={self.centroids}")
+        self.boundaries = uniform_filter1d(self.centroids, size=2, origin=-1)[:-1]
+        self.boundaries = np.concatenate(([0], self.boundaries, [256]))
+        logger.info(f"boundaries={self.boundaries}")
+
+    def _compute_centroids(self, counts):
+        '''Compute the centroid of each bin.'''
+        centroids = []
+        bin_size = self.Q_step
+        logger.info(f"bin_size={bin_size}")
+        for i in range(self.N_bins):
+            b_i = i*bin_size
+            b_i_1 = (i+1)*bin_size
+            logger.debug(f"b_i={b_i} b_i_1={b_i_1}")
+            # See from scipy.ndimage import center_of_mass
+            mass = np.sum([j*counts[j] for j in range(b_i, b_i_1)])
+            total_counts_in_bin = np.sum([counts[j] for j in range(b_i, b_i_1)])
+            centroid = mass/total_counts_in_bin
+            logger.debug(f"centroid={centroid}")
+            centroids.append(centroid)
+        self.centroids = np.array(centroids)
+        logger.info(f"centroids={self.centroids}")
+
+    def encode(self, x):
+        '''Find the quantization indexes for the signal <x>.
+
+        '''
+        k = np.searchsorted(self.boundaries, x)
         return k
 
     def decode(self, k):
@@ -128,12 +102,12 @@ class LloydMax_Quantizer(Quantizer):
         <k>.
 
         '''
-        y = self.centers[k]
+        y = self.centroids[k]
         return y
 
     def get_representation_levels(self):
         '''In a Lloyd-Max quantizer the representation levels are the
-        centroids computed by the classifier (K-Means).
+        centroids.
 
         '''
-        return self.centers
+        return self.centroids
